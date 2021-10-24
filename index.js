3664368fa68830ac6f3b1a0113f91e5402d74343
Shao <shaotang.yen@gmail.com>
2021-10-25 06:58:00 +1100

    added inquirer

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
